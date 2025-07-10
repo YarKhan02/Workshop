e55b39e892ddd76dc34ded2d8687244545170a0f
Hammad449 <shsmmad2006@gmail.com>
2025-07-10 17:58:47 +0500

    Finish merge from making-models into main and update car serializer and view

--- backend/workshop/serializers/car_serializer.py
+++ backend/workshop/serializers/car_serializer.py
@@ -13,3 +13,23 @@ class CarSerializer(serializers.ModelSerializer):
             'color',
             'vin',
         ]
+
+class DetailSerializer(serializers.ModelSerializer):
+    customer_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Car
+        fields = [
+            'id',
+            'make',
+            'model',
+            'year',
+            'license_plate',
+            'color',
+            'vin',
+            'customer_name',
+        ]
+    def get_customer_name(self, obj):
+        first_name = obj.customer.first_name
+        last_name = obj.customer.last_name
+        return f"{first_name} {last_name}" if first_name and last_name else "No Customer"
--- backend/workshop/views/car_view.py
+++ backend/workshop/views/car_view.py
@@ -4,13 +4,26 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from ..models.car import Car
-from ..serializers.car_serializer import CarSerializer 
+from ..serializers.car_serializer import CarSerializer, DetailSerializer
 
 class CarView(viewsets.ViewSet):
 
-    @action(detail = False, methods = ['get'], url_path = 'details')
+    @action(detail=False, methods=['get'], url_path='details')
     def car_details(self, request):
+        """
+        Basic car details without customer info.
+        """
         queryset = Car.objects.all()
-        serializer = CarSerializer(queryset, many = True)
+        serializer = CarSerializer(queryset, many=True)
         return Response(serializer.data)
-        
+
+    @action(detail=False, methods=['get'], url_path='with-customer')
+    def car_with_customer_name(self, request):
+        """
+        Car details including only the customer name.
+        """
+        queryset = Car.objects.select_related('customer').all()
+        serializer = DetailSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
